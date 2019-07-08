1f6eea8b9b2156d1175b9bfa937f914445ca5ddb
Akash <akash.h.r@sap.com>
2019-07-08 08:37:08 +0530

    Py game template

---
+++ Python/pygame_eg/gameTemlate.py
@@ -0,0 +1,26 @@
+#create a template for the game
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+#display surface
+width=800
+height=600
+game_display = pygame.display.set_mode((width, height))
+
+#title bar
+pygame.display.set_caption('The Game')
+
+#event handler for game loop
+def eventHandler():
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and (event.key == K_ESCAPE or event.key == K_q)):
+            pygame.quit()
+            quit()
+
+#game loop
+while True:
+    eventHandler()
+    pygame.display.update()
---
+++ Python/pygame_eg/steps.py
@@ -0,0 +1,14 @@
+# 1. import pygame
+    # i=pygame.init()
+    # print(i)
+
+# 2. set display
+# 3. update display 
+    #updating display
+    #filp()-updates entire display /update()-updates portion of display
+    #pygame.display.flip()
+
+# 4. game loop
+    #print(event)
+    #quitting (press X/key down)
+        
